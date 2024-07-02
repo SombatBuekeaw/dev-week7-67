2ed9edf51e93a3d35a30ffe51505abb140f67dd8
SombatBuekeaw <Sombat073855@gmail.com>
2024-07-02 13:51:05 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Welcom to Deploying a vue.js with Docker</h1>
+    <br />
+    <h2>My name is:{{name}}</h2>
+    <h2>My college is:{{college}}</h2>
+  </div>
+</template>
+
+<script setup>
+const name ="SOMBAT"
+const college = "วิทยาลัยเทคนิคตราด"
+</script>
+
+<style lang="scss" scoped>
+
+</style>
